7d8509c4178613f1dcf2611a243f051685260661
anthonyero <anthonyero@gmail.com>
2024-05-14 21:45:07 -0500

    Defined the addUser and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,9 +6,34 @@ const resolvers = {
 		me: async (parent, args, context) => {
 			if (context.user) {
 				return User.findOne({ _id: context.user._id }).populate('savedBooks');
-
 			}
-		}
+			throw AuthenticationError;
+	},
+
+	Mutation: {
+		addUser: async (parent, { username, email, password }) => {
+			const user = await User.create({ username, email, password });
+			const token = signToken(user);
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw AuthenticationError;
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw AuthenticationError;
+			}
+
+			const token = signToken(user);
+
+			return { token, user };
+		},
+		
 	}
 }
 
